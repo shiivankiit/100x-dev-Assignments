af2d2ca1d7e0de351bd2a24827b044803d8d0922
shiivankiit <shivankit7557@gmail.com>
2026-07-24 23:47:53 +0530

    Bg-color-changer

---
+++ Bootcamp-1.0 Assignments/Week-01/assignment-2/easy/bg-color-changer/script.js
@@ -0,0 +1,11 @@
+document.getElementById("red-btn").addEventListener("click",function(){
+    document.body.style.backgroundColor='red';
+})
+
+document.getElementById("yllw-btn").addEventListener("click",function(){
+    document.body.style.backgroundColor='yellow';
+})
+
+document.getElementById("blu-btn").addEventListener("click",function(){
+    document.body.style.backgroundColor='blue';
+})
