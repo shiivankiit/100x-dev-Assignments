0a8d99a4a2abda52838ca1712cd3289cc63ad26f
shiivankiit <shivankit7557@gmail.com>
2026-09-08 20:22:28 +0530

    DSA

---
+++ Bootcamp-1.0 Assignments/Week-02/js-built-ins/problems/hard/wordCompression.js
@@ -0,0 +1,53 @@
+/*
+  Write a function `compressWords` which takes an array of strings as input and returns a new array with consecutive duplicate elements compressed. If an element appears consecutively, it is replaced by the element followed by the count of its occurrences.
+
+  Example:
+  - Input: ["apple", "apple", "banana", "banana", "banana", "cherry", "apple", "apple"]
+  - Output: ["apple2", "banana3", "cherry", "apple2"]
+
+  - Input: ["cat", "dog", "dog", "dog", "cat"]
+  - Output: ["cat", "dog3", "cat"]
+
+  - Input: ["one", "two", "three"]
+  - Output: ["one", "two", "three"]
+
+  - Input: []
+  - Output: []
+
+  Note:
+  - The function should handle empty arrays and arrays with no consecutive duplicates.
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-compressWord`
+*/
+
+
+function compressWords(arr) {
+    let result = [];
+
+    if (arr.length === 0) {
+        return result;
+    }
+
+    let count = 1;
+
+    for (let i = 1; i <= arr.length; i++) {
+
+        if (arr[i] === arr[i - 1]) {
+            count++;
+        } else {
+            if (count === 1) {
+                result.push(arr[i - 1]);
+            } else {
+                result.push(arr[i - 1] + count);
+            }
+
+            count = 1;
+        }
+    }
+
+    return result;
+}
+
+
+module.exports = compressWords;
---
+++ Bootcamp-1.0 Assignments/Week-02/js-built-ins/problems/medium/stringCompression.js
@@ -0,0 +1,41 @@
+/*
+  Write a function `compression` which takes a string as input and returns a compressed version of the string. The compression is done by replacing consecutive repeating characters with the character followed by the count of repetitions. If a character does not repeat, it is not followed by a count.
+
+  Example:
+  - Input: "aaabbbbcccvvmm"
+  - Output: "a3b4c3v2m2"
+
+  - Input: "abc"
+  - Output: "abc"
+
+  - Input: "aabbcc"
+  - Output: "a2b2c2"
+
+  - Input: ""
+  - Output: ""
+
+  Note:
+  - The function should work for any alphanumeric string.
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-compressString`
+*/
+function compression(str) {
+  // Your code here
+     let result = "";
+    let count = 1;
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            result += str[i] + count;
+            count = 1;
+        }
+    }
+
+    return result;
+}
+console.log(compressString("aaabbbbcccvvmm"));
+module.exports = compression;
